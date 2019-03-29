734148b50611e181ab825a96bca81d1ae305b3e7
Xinyiguo2 <xinyig2@illinois.edu>
2019-03-29 03:50:27 -0500

    add db creds

--- config/secrets.js
+++ config/secrets.js
@@ -3,5 +3,5 @@
  */
 module.exports = {
     token : "secret-starter-mern",
-    mongo_connection : "YOUR URL TO MONGODB"
+    mongo_connection : "mongodb+srv://legit_xinyi:itsFriday0329@cluster0-bjnl4.mongodb.net/test?retryWrites=true"
 };
