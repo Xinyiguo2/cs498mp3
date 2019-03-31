8f6a8a2a457081d6da1028f17681f1bfb172cbab
Xinyiguo2 <xinyig2@illinois.edu>
2019-03-31 18:24:48 -0500

    change the url for users

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,5 @@
  */
 module.exports = function (app, router) {
     app.use('/api', require('./home.js')(router));
-    app.use('/users', require('./users.js')(router));
+    app.use('/api/users', require('./users.js')(router));
 };
